7faa36ca622997d4a4ba0c0eedb1e1cec861c102
surya277 <ssurya0309@gmail.com>
2023-01-29 11:03:44 -0500

    Manager Update

--- DragonFly Engine/Manager.cpp
+++ DragonFly Engine/Manager.cpp
@@ -1,1 +1,46 @@
 #include "Manager.h"
+
+
+
+df::Manager::Manager() {
+
+}
+
+
+int df::Manager::startUp() {
+	m_is_started = true;
+	return 0;
+}
+
+
+
+void df::Manager::shutDown() {
+	m_is_started = false;
+}
+
+
+
+bool df::Manager::isStarted() const {
+	return m_is_started;
+}
+
+
+void df::Manager::setType(std::string type) {
+	m_type = "Manager";
+	//m_type = type;
+}
+
+
+
+std::string df::Manager::getType() const{
+	return m_type;
+}
+
+
+df::Manager::~Manager() {
+
+}
+
+
+
+
--- DragonFly Engine/Manager.h
+++ DragonFly Engine/Manager.h
@@ -1,5 +1,38 @@
 #pragma once
-class Manager
-{
-};
+#include <string>
 
+
+namespace df {
+
+	class Manager
+	{
+
+	private:
+		std::string m_type;
+		bool m_is_started;
+
+	protected:
+		// Set type identifier of Manager
+		// Is protected since we want only the base class and its derived classes to be able set the type
+		void setType(std::string type);		
+
+	public:
+		Manager();
+		virtual ~Manager();
+
+		//Get type identifier of Manager
+		std::string getType() const;
+
+		//Startup Manager
+		virtual int startUp();
+		
+
+		//Shutdown Manager
+		virtual void shutDown();
+
+		//returns true if startUp was executed OK else false
+		bool isStarted() const;
+
+	};
+
+}
